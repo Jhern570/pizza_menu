2f2d25a063ded6161f17bc89289a3deb71dc4e66
julio-wwp <julioh@waterwayplastics.com>
2026-04-14 15:40:19 -0700

    added ternary for sold out

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ function App(){
 
 
 function Header(){
-    // const style = { color: "red", fontSize: '48px', textTransform: 'uppercase' };
+    
     const style ={};
 
     return (
@@ -27,61 +27,68 @@ function Header(){
 );}
 
 function Menu(){
+
+    const pizzas = pizzaData;
+    const numPizzas = pizzas.length;
+
     return (
         <main className="menu">
             <h2>Our menu</h2>
 
-            <ul className="pizzas">
+    {numPizzas > 0 ? (
+
+     <ul className="pizzas">
                 {pizzaData.map((pizza) => (
                     <Pizza 
-                        // key={pizza.namew} 
-                        // name={pizza.name}
-                        // ingredients={pizza.ingredients}
-                        // photoName={pizza.photoName}
-                        // price={pizza.price} 
+                        
                         key={pizza.name}
                         pizzaObj={pizza}
                     />
                 ))}
             </ul>
-
-            {/* <Pizza 
-                name='Pizza Spinaci' 
-                ingredient='Tomato, mozarella, spinach, and ricotta cheese' 
-                photoName='pizzas/spinaci.jpg' 
-                price={10}
-                />
-            <Pizza 
-                name='Pizza Funghi' 
-                ingredient='Tomato, mushrooms' 
-                photoName='pizzas/funghi.jpg' 
-                price={12}
-                /> */}
+    ) : (
+        <p>We're are still working on our menu. Please come back later</p>
+    )}
         </main>
     );
 }
 
 function Footer(){
         const hour = new Date().getHours();
-        const openHour = 12;
+        const openHour = 8;
         const closeHour = 22;
 
         const isOpen = hour >= openHour && hour <= closeHour;
-        // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
-        // else alert("Sorry we're close");
-
-        return <footer className="footer">{new Date().toLocaleTimeString()}. We're currently open</footer>
-
+    
+        return (
+        <footer className="footer">
+            {isOpen ? (
+                <Order closeHour={closeHour} openHour={openHour}/>
+            ) : (
+                <p>We are happy to assist you between {openHour}:00 and {closeHour}:00</p>
+            )}
+        </footer>
+        );
 }
-function Pizza(props){
+
+function Order({closeHour, openHour}){
     return (
 
-        <li className='pizza'>
-            <img src={props.pizzaObj.photoName} atl={props.pizzaObj.name}/>
+        <div className="order">
+            <p>We're open from {openHour}:00 to {closeHour}:00. Come visit us or order online</p>
+            <button className="btn">Order</button>
+        </div>
+    );
+}
+function Pizza({pizzaObj}){
+    return (
+
+        <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+            <img src={pizzaObj.photoName} atl={pizzaObj.name}/>
             <div>
-                <h3>{props.pizzaObj.name}</h3>
-                <p>{props.pizzaObj.ingredients}s</p>
-                <span>{props.pizzaObj.price + 3}</span>
+                <h3>{pizzaObj.name}</h3>
+                <p>{pizzaObj.ingredients}s</p>
+                <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
             </div>
         </li>
     );
